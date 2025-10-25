5982d1eab634b9d800dd277dfd2ba43a2ec852f6
Noah Jutz <noah@noahjutz.com>
2025-10-25 15:43:51 +0200

    Add await() overload with timeout

--- src/MyCountDownLatch.java
+++ src/MyCountDownLatch.java
@@ -1,3 +1,11 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.TemporalAmount;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 public class MyCountDownLatch {
   private int count;
 
@@ -20,5 +28,28 @@ public class MyCountDownLatch {
       } catch (InterruptedException e) {
       }
     }
+  }
+
+  public synchronized boolean await(long timeout, TimeUnit unit) {
+    var timer = new Timer();
+    var targetTime = System.currentTimeMillis() + unit.toMillis(timeout);
+    var task = new TimerTask() {
+      @Override
+      public void run() {
+        MyCountDownLatch.this.notifyAll();
+      }
+    };
+
+    timer.schedule(task, targetTime);
+
+    while (System.currentTimeMillis() <= targetTime && count > 0) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    return System.currentTimeMillis() <= targetTime;
   }
 }
